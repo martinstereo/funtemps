e034c21391680e908c1a5d8dcc478f06645dfbb9
naausicaa <martinsteiro02@gmail.com>
2023-02-10 14:18:54 +0100

    implementert logikk for flagg og kall til funksjoner

--- main.go
+++ main.go
@@ -3,42 +3,44 @@ package main
 import (
 	"flag"
 	"fmt"
+
+	"github.com/naausicaa/funtemps/conv"
 )
 
 // Definerer flag-variablene i hoved-"scope"
-var fahr float64
-var out string
-var funfacts string
+var (
+	fahr     float64
+	kelvin   float64
+	celsius  float64
+	out      string
+	funfacts string
+)
 
 // Bruker init (som anbefalt i dokumentasjonen) for å sikre at flagvariablene
 // er initialisert.
+
 func init() {
 
-	/*
-	   Her er eksempler på hvordan man implementerer parsing av flagg.
-	   For eksempel, kommando
-	       funtemps -F 0 -out C
-	   skal returnere output: 0°F er -17.78°C
-	*/
-
 	// Definerer og initialiserer flagg-variablene
-	flag.Float64Var(&fahr, "F", 0.0, "temperatur i grader fahrenheit")
-	// Du må selv definere flag-variablene for "C" og "K"
-	flag.StringVar(&out, "out", "C", "beregne temperatur i C - celsius, F - farhenheit, K- Kelvin")
-	flag.StringVar(&funfacts, "funfacts", "sun", "\"fun-facts\" om sun - Solen, luna - Månen og terra - Jorden")
+	flag.Float64Var(&fahr, "F", 0.0, "temperatur i grader Fahrenheit")
+	flag.Float64Var(&kelvin, "K", 0.0, "temperatur i grader Kelvin")
+	flag.Float64Var(&celsius, "C", 0.0, "temperatur i grader Celsius")
+
+	flag.StringVar(&out, "out", "C", "beregne temperatur i F - farhenheit")
+
+	// flag.StringVar(&funfacts, "funfacts", "sun", "\"fun-facts\" om sun - Solen, luna - Månen og terra - Jorden")
+
 	// Du må selv definere flag-variabelen for -t flagget, som bestemmer
 	// hvilken temperaturskala skal brukes når funfacts skal vises
-
 }
 
 func main() {
-
+	fmt.Println("Starting the application...")
 	flag.Parse()
 
 	/**
 	    Her må logikken for flaggene og kall til funksjoner fra conv og funfacts
-	    pakkene implementeres.
-
+	    pakkene implementere
 	    Det er anbefalt å sette opp en tabell med alle mulige kombinasjoner
 	    av flagg. flag-pakken har funksjoner som man kan bruke for å teste
 	    hvor mange flagg og argumenter er spesifisert på kommandolinje.
@@ -52,31 +54,48 @@ func main() {
 	    disse tre kan brukes med -out, men ikke med -funfacts
 	    -funfacts kan brukes kun med -t
 	    ...
-	    Jobb deg gjennom alle tilfellene. Vær obs på at det er en del sjekk
-	    implementert i flag-pakken og at den vil skrive ut "Usage" med
-	    beskrivelsene av flagg-variablene, som angitt i parameter fire til
-	    funksjonene Float64Var og StringVar
 	*/
 
-	// Her er noen eksempler du kan bruke i den manuelle testingen
-	fmt.Println(fahr, out, funfacts)
-
-	fmt.Println("len(flag.Args())", len(flag.Args()))
-	fmt.Println("flag.NFlag()", flag.NFlag())
-
-	fmt.Println(isFlagPassed("out"))
-
-	// Eksempel på enkel logikk
+	//Fahrenheit til Celsius
 	if out == "C" && isFlagPassed("F") {
-		// Kalle opp funksjonen FahrenheitToCelsius(fahr), som da
-		// skal returnere °C
-		fmt.Println("0°F er -17.78°C")
+		celsius = conv.FahrenheitToCelsius(fahr)
+		fmt.Println(fahr, "°F er ", celsius, "°C")
+	}
+
+	//Fahrenheit til Kelvin
+	if out == "K" && isFlagPassed("F") {
+		kelvin = conv.FahrenheitToKelvin(fahr)
+		fmt.Println(fahr, "°F er ", kelvin, "°K")
+	}
+
+	//Celsius til Fahrenheit
+	if out == "F" && isFlagPassed("C") {
+		fahr = conv.CelsiusToFahrenheit(celsius)
+		fmt.Println(celsius, "°C er ", fahr, "°F")
+	}
+
+	//Celsius til Kelvin
+	if out == "K" && isFlagPassed("C") {
+		kelvin = conv.CelsiusToKelvin(celsius)
+		fmt.Println(celsius, "°C er ", kelvin, "°K")
+	}
+
+	//Kelvin til Fahrenheit
+	if out == "F" && isFlagPassed("K") {
+		fahr = conv.KelvinToFahrenheit(kelvin)
+		fmt.Println(kelvin, "°K er ", fahr, "°F")
+	}
+
+	//Kelvin til Celsius
+	if out == "C" && isFlagPassed("K") {
+		celsius = conv.KelvinToCelsius(kelvin)
+		fmt.Println(kelvin, "°K er ", celsius, "°C")
 	}
 
 }
 
 // Funksjonen sjekker om flagget er spesifisert på kommandolinje
-// Du trenger ikke å bruke den, men den kan hjelpe med logikken
+
 func isFlagPassed(name string) bool {
 	found := false
 	flag.Visit(func(f *flag.Flag) {
